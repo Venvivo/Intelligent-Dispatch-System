6909d9de4b6435a8bcd001ee71638513b75bf4c5
zhangwei <hrj900824>
2018-11-01 16:34:20 +0800

    fixed

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // 懒加载方式，当路由被访问的时候才加载对应组件
-const Index = resolve => require(['@/components/PublicIndex'], resolve);
+const Index = resolve => require(['@/components/Index'], resolve);
 
 
 Vue.use(Router);
